2b02807867899bfc8c63838049d6d013fc50d058
Alef Ramos <victorraoli@outlook.com>
2023-09-13 15:11:49 -0300

    First commit, sections home page finished

---
+++ js/main.js
@@ -0,0 +1,64 @@
+/*=== Swiper Gallery ===*/
+var swiperCategories = new Swiper(".categories-container", {
+  spaceBetween: 24,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    758: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1400: {
+      slidesPerView: 6,
+      spaceBetween: 24,
+    },
+  },
+});
+/*=== Product Tabs ===*/
+const tabs = document.querySelectorAll("[data-target]"),
+  tabContents = document.querySelectorAll("[content]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("active-tab");
+    });
+    target.classList.add("active-tab");
+
+    tabs.forEach((tab) => {
+      tab.classList.remove("active-tab");
+    });
+    tab.classList.add("active-tab");
+  });
+});
+/*=== Swiper Products ===*/
+var swiperProducts = new Swiper(".new-container", {
+  spaceBetween: 24,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    758: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1400: {
+      slidesPerView: 4,
+      spaceBetween: 24,
+    },
+  },
+});
